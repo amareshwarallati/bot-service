1caebde30b28fa75396d80ed69a0407c78ea326a
Amareshwar Reddy Allati <amar@Amareshwars-MacBook-Pro.local>
2017-09-05 14:10:14 +0530

    API.AI support addtion

--- src/main/java/com/wow/botservice/resources/Authenticator.java
+++ src/main/java/com/wow/botservice/resources/Authenticator.java
@@ -1,5 +1,11 @@
 package com.wow.botservice.resources;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.wow.botservice.configuration.ApplicationProperties;
 import com.wow.botservice.exception.CompareException;
 import com.wow.botservice.model.ErrorResponse;
@@ -13,6 +19,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+import java.io.IOException;
+
 @RestController
 public class Authenticator {
 
@@ -25,7 +34,8 @@ public class Authenticator {
 	@Autowired
     private ApplicationProperties properties;
 
-	@RequestMapping(value = "rest/purchaseOrder", method = RequestMethod.GET)
+	//Get status of the purchaser order using GET method for simple NON- API.AI clients.
+	@RequestMapping(value = "rest/getStatus", method = RequestMethod.GET)
 	public ResponseEntity<PurchaseOrder> getPOStatus(@RequestParam("poNbr") final String poNbr) throws CompareException {
 		PurchaseOrder po = new PurchaseOrder();
 		//po.setStatus("Closed");
@@ -33,11 +43,33 @@ public class Authenticator {
 		po = this.compareService.getPurchaseOrder(po);
 		return new ResponseEntity<PurchaseOrder>(po, HttpStatus.OK);
 	}
-	
-	@RequestMapping(value = "rest/purchaseOrder", method = RequestMethod.POST)
+
+	//Add purchase order to database.
+	@RequestMapping(value = "rest/addOrder", method = RequestMethod.POST)
 	public ResponseEntity<String> addPurchaseOrder(@RequestBody final PurchaseOrder po) throws CompareException {
 		String res = this.compareService.addPurchaseOrder(po);
 		return new ResponseEntity<String>(res, HttpStatus.CREATED);
+	}
+
+	// Get status of purchse order based on the order Id - For API.AI reuests.
+	@RequestMapping(value = "rest/getStatus", method = RequestMethod.POST)
+	public ResponseEntity<String> addPurchaseOrder(@RequestBody String postMsg) throws JsonParseException, IOException,CompareException {
+		ObjectMapper om = new ObjectMapper();
+		JsonNode jNode = om.readTree(postMsg);
+		PurchaseOrder po = new PurchaseOrder();
+		po.setPoNbr(jNode.get("result").get("parameters").get("poNbr").asText());
+		po = this.compareService.getPurchaseOrder(po);
+
+		ObjectNode jn = JsonNodeFactory.instance.objectNode();
+		if(po != null && po.getPoNbr() != null) {
+			jn.put("poNbr", po.getPoNbr());
+			jn.put("status", po.getStatus());
+		}else{
+			jn.put("poNbr", jNode.get("result").get("parameters").get("poNbr").asText());
+			jn.put("status", "N/A");
+		}
+
+		return new ResponseEntity<String>(jn.toString(), HttpStatus.OK);
 	}
 	
 	@ExceptionHandler(CompareException.class)
